c8ff2a171cae72b48b6853e8fc86184e1061402b
dichloride <harbour258@outlook.com>
2026-01-04 16:42:38 +0800

    update

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(int i=0;i<4;++i) {
+            size*=shape_[i];
+            shape[i]=shape_[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -26,8 +30,38 @@ struct Tensor4D {
     // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
     // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
+    unsigned int get_index(unsigned int i0, unsigned int i1, unsigned int i2, unsigned int i3) const {
+        return i0 * (shape[1] * shape[2] * shape[3]) + 
+               i1 * (shape[2] * shape[3]) + 
+               i2 * shape[3] + 
+               i3;
+    }
+
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        for(unsigned int i0=0;i0<shape[0];++i0){
+            for(unsigned int i1=0;i1<shape[1];++i1){
+                for(unsigned int i2=0;i2<shape[2];++i2){
+                    for(unsigned int i3=0;i3<shape[3];++i3){
+                        unsigned int other_i0 = (others.shape[0] == 1) ? 0 : i0;
+                        unsigned int other_i1 = (others.shape[1] == 1) ? 0 : i1;
+                        unsigned int other_i2 = (others.shape[2] == 1) ? 0 : i2;
+                        unsigned int other_i3 = (others.shape[3] == 1) ? 0 : i3;
+                        
+                        // 计算1D数组中的索引
+                        unsigned int this_idx = get_index(i0, i1, i2, i3);
+                        unsigned int other_idx = others.get_index(other_i0, other_i1, other_i2, other_i3);
+                        
+                        // 执行加法
+                        data[this_idx] += others.data[other_idx];
+
+                    }
+                }
+            }
+        }
+            
+                
+                    
         return *this;
     }
 };
--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -18,6 +18,18 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    if (shape.empty()) {
+        return strides;
+    }
+
+    udim stride=1;
+    auto strides_it=strides.rbegin();
+    auto shape_it=shape.rbegin();
+    *strides_it++=stride;
+    for(;strides_it!=strides.rend();++strides_it,++shape_it) {
+        stride *= *shape_it;
+        *strides_it = stride;
+    }
     return strides;
 }
 
